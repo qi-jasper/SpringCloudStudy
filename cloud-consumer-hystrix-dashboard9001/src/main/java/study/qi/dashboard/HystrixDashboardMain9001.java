4efe84abdd2f41fb0ec8399f4dc669e68e0b4d86
qi <qi.jasper@qq.com>
2021-04-16 11:16:33 +0800

    feat[Hystrix]:Hystrix Dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/study/qi/dashboard/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package study.qi.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Description
+ * @Author qi
+ * @Date 2021/4/16 10:19
+ * @ClassName HystrixDashboardMain9001
+ **/
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
